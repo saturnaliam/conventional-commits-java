ca1eebd228030426f06490f3fcaf03fe1d888ba3
saturnaliam <luciacdev@gmail.com>
2024-06-26 23:47:10 -0500

    dumb json parsing

--- src/main/java/saturnaliam/convcommits/CommitTag.java
+++ src/main/java/saturnaliam/convcommits/CommitTag.java
@@ -8,4 +8,6 @@ public class CommitTag {
         this.tagName = tagName;
         this.tagDescription = tagDescription;
     }
+
+    public CommitTag() {}
 }
--- src/main/java/saturnaliam/convcommits/Main.java
+++ src/main/java/saturnaliam/convcommits/Main.java
@@ -1,14 +1,28 @@
 package main.java.saturnaliam.convcommits;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.lang.Object;
+import java.lang.Class;
+
 public class Main {
     public static void main(String[] args) {
-        CommitTag fix = new CommitTag("fix", "denotes a fix for an issue.");
-
         try {
-            Commit commit = new CommitBuilder().addTag(fix).addMessage("balling").addScope("intake").build();
-            System.out.println(commit.getMessage());
+            CommitTag[] tags = readCommitTags("hi.json");
+            System.out.println(tags[0].tagDescription);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private static CommitTag[] readCommitTags(String filename) throws IOException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            CommitTag[] tags = mapper.readValue(new File(filename), CommitTag[].class);
+            return tags;
+        } catch (IOException e) {
+            throw e;
         }
     }
 }
